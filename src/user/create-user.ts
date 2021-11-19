54b878a9797fa6a6a03cc8d68d737263d10f7088
Dixon Gunasekara <dixon@randomsoftware.net>
2021-11-20 00:30:55 +0530

    refactor: :rocket:

--- src/user/create-user.ts
+++ src/user/create-user.ts
@@ -1,4 +1,4 @@
-import "source-map-support/register";
+// import "source-map-support/register";
 import * as aws from "aws-sdk";
 import {
   APIGatewayProxyEvent,
--- src/welcome/app.ts
+++ src/welcome/app.ts
@@ -1,4 +1,4 @@
-import "source-map-support/register";
+// import "source-map-support/register";
 import {
   APIGatewayProxyEvent,
   APIGatewayEventRequestContext,
